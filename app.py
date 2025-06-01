f6b85c9babc783960e4d24977685956bdb140da1
Shubham Raj <shubmrj@gmail.com>
2025-06-02 01:54:27 +0530

    animation establish

--- app.py
+++ app.py
@@ -392,6 +392,11 @@ def index():
     logger.info('Home page accessed')
     return render_template('index.html')
 
+@app.route('/test-portfolio')
+def test_portfolio():
+    logger.info('Test portfolio page accessed')
+    return send_file('test-portfolio.html')
+
 @app.route('/blog')
 def blog():
     """Render the blog page"""
